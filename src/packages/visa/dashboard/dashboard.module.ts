9b2e53d1793ff3c8370f8d50811d679a30d4a64e
Brilian Rachmad Nurwachidin <76217952+brilianrn@users.noreply.github.com>
2026-03-23 21:46:00 +0700

    fix: dashboard

--- src/packages/visa/dashboard/dashboard.module.ts
+++ src/packages/visa/dashboard/dashboard.module.ts
@@ -2,8 +2,20 @@ import { Module } from '@nestjs/common';
 import { DashboardController } from './controller/dashboard.controller';
 import { DashboardUseCase } from './usecase/dashboard.usecase';
 import { DashboardRepository } from './repository/dashboard.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET') || 'secret',
+        signOptions: { expiresIn: '1d' },
+      }),
+    }),
+  ],
   controllers: [DashboardController],
   providers: [
     {
