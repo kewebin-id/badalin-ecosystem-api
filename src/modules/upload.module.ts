6b72c22c74b1178c243a04de49e3cd05ea7eccf5
Brilian Rachmad Nurwachidin <76217952+brilianrn@users.noreply.github.com>
2026-03-26 20:16:59 +0700

    fix: upload

--- src/modules/upload.module.ts
+++ src/modules/upload.module.ts
@@ -1,10 +1,22 @@
 import { OcrService } from '@/shared/services/ocr.service';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { UploadController } from '../packages/upload/controller/upload.controller';
 import { PrismaUploadRepository } from '../packages/upload/repository/upload.repository';
 import { UploadUseCase } from '../packages/upload/usecase/upload.usecase';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET') || 'secret',
+        signOptions: { expiresIn: '1d' },
+      }),
+    }),
+  ],
   controllers: [UploadController],
   providers: [
     OcrService,
