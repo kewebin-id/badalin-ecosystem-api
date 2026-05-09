a801f82bd804439bf06eee297f90dccccdd9f715
Brilian Rachmad Nurwachidin <76217952+brilianrn@users.noreply.github.com>
2026-05-09 21:24:52 +0700

    chore: add tour guide

--- src/packages/visa/provider/agency-settings/ports/agency-settings.repository.port.ts
+++ src/packages/visa/provider/agency-settings/ports/agency-settings.repository.port.ts
@@ -1,8 +1,8 @@
-import { Agency } from '@prisma/client';
+import { Agency, Prisma } from '@prisma/client';
 
 export interface IAgencySettingsRepository {
   findById(id: string): Promise<Agency | null>;
   findBySlug(slug: string): Promise<Agency | null>;
   create(data: Partial<Agency>): Promise<Agency>;
-  update(id: string, data: Partial<Agency>, oldSlug?: string): Promise<Agency>;
+  update(id: string, data: Prisma.AgencyUpdateInput, oldSlug?: string): Promise<Agency>;
 }
--- src/packages/visa/provider/agency-settings/ports/agency-settings.usecase.port.ts
+++ src/packages/visa/provider/agency-settings/ports/agency-settings.usecase.port.ts
@@ -2,9 +2,17 @@ import { IUsecaseResponse } from '@/shared/utils';
 import { Agency } from '@prisma/client';
 import { UpdateAgencySettingsDto } from '../dto/agency-settings.dto';
 
+export interface IAgencyResponse extends Agency {
+  isSlugSetup: boolean;
+  newToken?: string;
+}
+
 export interface IAgencySettingsUseCase {
-  getAgencyData(providerId: string): Promise<IUsecaseResponse<Agency>>;
+  getAgencyData(providerId: string): Promise<IUsecaseResponse<IAgencyResponse>>;
   checkSlugAvailability(slug: string): Promise<IUsecaseResponse<{ available: boolean }>>;
-  updateAgencySettings(providerId: string, dto: UpdateAgencySettingsDto): Promise<IUsecaseResponse<Agency>>;
+  updateAgencySettings(
+    providerId: string,
+    dto: UpdateAgencySettingsDto,
+  ): Promise<IUsecaseResponse<IAgencyResponse>>;
   validateSession(providerId: string): Promise<IUsecaseResponse<{ valid: boolean }>>;
 }
