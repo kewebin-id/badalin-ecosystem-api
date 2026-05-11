4f53fdad36b4eabe2589faf52b14b9e4812eacaa
Brilian Rachmad Nurwachidin <76217952+brilianrn@users.noreply.github.com>
2026-05-11 14:27:50 +0700

    fix: refund

--- src/packages/visa/provider/refund/controller/refund.controller.ts
+++ src/packages/visa/provider/refund/controller/refund.controller.ts
@@ -1,4 +1,15 @@
-import { Body, Controller, Get, Inject, Param, Post, Query, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Inject,
+  Param,
+  Post,
+  Query,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { IRefundUseCase } from '../ports/refund.usecase.port';
 import { JwtAuthGuard } from '@/shared/guards/jwt-auth.guard';
 import { UserContext } from '@/shared/decorators/user-context.decorator';
@@ -7,6 +18,8 @@ import { SettleRefundDto } from '../dto/refund.dto';
 import { EVisaRoutes } from '@/shared/constants';
 import { SlugGuard } from '@/shared/guards/slug.guard';
 import { ReservedWordGuard } from '@/shared/guards/reserved-word.guard';
+import { response } from '@/shared/utils/rest-api/response';
+import { Response } from 'express';
 
 @Controller(EVisaRoutes.PROVIDER_REFUND)
 @UseGuards(JwtAuthGuard, SlugGuard, ReservedWordGuard)
@@ -19,16 +32,23 @@ export class RefundController {
   @Get()
   async getRefundList(
     @UserContext() ctx: IUserContext,
+    @Res() res: Response,
     @Query('page') page: number = 1,
     @Query('limit') limit: number = 10,
     @Query('search') search?: string,
   ) {
-    const data = await this.useCase.getRefundList(ctx, page, limit, search);
-    return {
-      code: 200,
-      message: 'Success!',
-      data,
-    };
+    const result = await this.useCase.getRefundList(ctx, page, limit, search);
+
+    if (result.error) {
+      return response[result.error.code || HttpStatus.INTERNAL_SERVER_ERROR](res, {
+        message: result.error.message,
+      });
+    }
+
+    return response[HttpStatus.OK](res, {
+      message: 'Refund list fetched successfully',
+      data: result.data,
+    });
   }
 
   @Post(':id/settle')
@@ -36,7 +56,19 @@ export class RefundController {
     @Param('id') id: string,
     @Body() dto: SettleRefundDto,
     @UserContext() ctx: IUserContext,
+    @Res() res: Response,
   ) {
-    return this.useCase.settleRefund(id, dto.file, ctx);
+    const result = await this.useCase.settleRefund(id, dto.file, ctx);
+
+    if (result.error) {
+      return response[result.error.code || HttpStatus.INTERNAL_SERVER_ERROR](res, {
+        message: result.error.message,
+      });
+    }
+
+    return response[HttpStatus.OK](res, {
+      message: 'Refund settled successfully',
+      data: result.data,
+    });
   }
 }
